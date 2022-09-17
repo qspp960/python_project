6bb3da2b526d2ab0bc4271536f35cebcac94f1ac
qspp960 <qspp@daum.net>
2022-09-17 17:34:23 +0900

    snake game

---
+++ snake_game/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down,"s")
+screen.onkey(snake.left,"a")
+screen.onkey(snake.right,"d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
+
---
+++ snake_game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake():
+    def __init__(self):
+        self.segment = []
+        self.create_snake()
+        self.head = self.segment[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle()
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            new_segment.shape("square")
+            self.segment.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segment)-1,0,-1):
+            new_x = self.segment[seg_num-1].xcor()
+            new_y = self.segment[seg_num-1].ycor()
+            self.segment[seg_num].goto(new_x,new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
