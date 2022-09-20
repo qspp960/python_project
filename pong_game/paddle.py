a03d6cc1325a16fb7df791088d1df5a2b94e5f8d
qspp960 <qspp@daum.net>
2022-09-20 22:33:18 +0900

    pong game

---
+++ pong_game/paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self,position):
+        super().__init__()
+        self.goto(position[0], position[1])
+        self.shape("square")
+        self.shapesize(stretch_wid=5.0,stretch_len=1.0)
+        self.color("white")
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.penup()
+        self.goto(self.xcor(),new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.penup()
+        self.goto(self.xcor(),new_y)
+
+
+
