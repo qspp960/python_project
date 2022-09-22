9665836d9516bf390dc0776d16c014ee64ac32c1
qspp960 <qspp@daum.net>
2022-09-22 23:33:33 +0900

    us states game

---
+++ us-states-game-start/main.py
@@ -0,0 +1,26 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",prompt="What's the another states name?").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
