9d70533af00d1912f45f1ff589482abeefc10f05
qspp960 <qspp@daum.net>
2022-09-27 20:40:14 +0900

    예외 처리 추가

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,22 +1,22 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+#student_dict = {
+#    "student": ["Angela", "James", "Lily"],
+#    "score": [56, 76, 98]
+#}
 
 #Looping through dictionaries:
-for (key, value) in student_dict.items():
+#for (key, value) in student_dict.items():
     #Access key and value
-    pass
+#    pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+#student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
+#for (index, row) in student_data_frame.iterrows():
     #Access index and row
     #Access row.student or row.score
-    if row.student == "Angela":
-        print(row.score)
+#    if row.student == "Angela":
+        #print(row.score)
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -25,10 +25,29 @@ for (index, row) in student_data_frame.iterrows():
 #{"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dic = {row.letter:row.code for (index,row) in data.iterrows()}
-print(data_dic)
+#print(data_dic)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [data_dic[letter] for letter in word]
-print(output_list)
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [data_dic[letter] for letter in word]
+    except KeyError:
+        print("sorry, only letters in the alphabet please")
+    else:
+        print(output_list)
+        break
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [data_dic[letter] for letter in word]
+    except KeyError:
+        print("sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
+
