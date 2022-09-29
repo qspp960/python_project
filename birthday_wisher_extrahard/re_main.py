ea4e0fb044de27fb624b9fc800282e0c8fdeed86
qspp960 <qspp@daum.net>
2022-09-29 20:07:25 +0900

    smtplib refactoring

---
+++ birthday_wisher_extrahard/re_main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+today = datetime.now()
+today_tuple = (today.month,today.day)
+my_email = "smtptest960@gmail.com"
+password = "huezptrtjmwjpdow"
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row["month"], data_row["day"]) : data_row for (index,data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[name]",birthday_person["name"])
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=birthday_person['email'],
+                                msg=f"Subject:hello\n\n {contents}")
