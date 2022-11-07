291a464bffdf8926c3999ca217ae64cc6456782d
kimeuihyun <qspp@daum.net>
2022-11-07 06:47:09 -0800

    flask request (GET,POST)

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+from flask import request
 
 POST_API_URL = 'https://api.npoint.io/5a9a97267b128ec41bd8'
 response = requests.get(url=POST_API_URL)
@@ -11,9 +12,22 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html',post_data=post_data)
 
+
 @app.route("/contact")
 def contact_page():
     return render_template('contact.html')
+
+
+@app.route("/contact",methods=["GET","POST"])
+def receive_contact_data():
+    contact_data = {}
+    contact_data['name'] = request.form.get('name')
+    contact_data['email'] = request.form.get('email')
+    contact_data['phone'] = request.form.get('phone')
+    contact_data['message'] = request.form.get('message')
+    print(contact_data)
+    return render_template('contact.html',contact_data=contact_data)
+
 
 @app.route("/about")
 def about_page():
